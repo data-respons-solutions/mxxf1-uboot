9d9809826392a18f234f84edb664a101dfeb24d3
hcl <hcl@datarespons.no>
2013-08-28 16:37:10 +0200

    Rename files, pull in DDR3 calibration data

--- board/datarespons/ocas-rdb/ocas-fpga.c
+++ board/datarespons/ocas-rdb/ocas-fpga.c
@@ -26,6 +26,7 @@
  * Altera FPGA configuration support
  */
 
+#include <asm/errno.h>
 #include <common.h>
 #include <altera.h>
 #include <ACEX1K.h>
@@ -33,15 +34,14 @@
 #include <spi.h>
 #include <asm/imx-common/iomux-v3.h>
 #include <asm/gpio.h>
-#include "fpga.h"
+#include <fpga.h>
+
+#include "ocas-pins.h"
 
 DECLARE_GLOBAL_DATA_PTR;
 
 #define CONFIG_FPGA_COUNT 1
 
-#define GPIO_FPGA_DONE			IMX_GPIO_NR(5, 14)
-#define GPIO_FPGA_STATUSn		IMX_GPIO_NR(5, 15)
-#define GPIO_FPGA_CONFIGn		IMX_GPIO_NR(5, 16)
 
 /* Plattforminitializations */
 /* Here we have to set the FPGA Chain */
@@ -54,90 +54,106 @@ int gpio_direction_input(unsigned gpio);
 int gpio_direction_output(unsigned gpio, int value);*/
 
 
-int fpga_pre_fn(int cookie)
+static int fpga_pre_fn(int cookie)
 {
     /* FPGA Configuration */
-	/* First we set STATUS to 1 then as an input */
-    gpio_set_value(GPIO_FPGA_STATUSn, 1);
-    gpio_direction_input(GPIO_FPGA_STATUSn);
-
-	/* output */
-    gpio_direction_output(GPIO_FPGA_CONFIGn, 1);
-    gpio_set_value(GPIO_FPGA_CONFIGn, 1);
-
-   	/* input */
-    gpio_direction_input(GPIO_FPGA_DONE);
-    gpio_set_value(GPIO_FPGA_DONE, 0);
-
+    printf("%s: FPGA_STATUSn %d\n", __func__, gpio_get_value(GPIO_FPGA_STATUSn));
+    printf("%s: FPGA_DONE %d\n", __func__, gpio_get_value(GPIO_FPGA_DONE));
 	return FPGA_SUCCESS;
 }
 
 /* Set the state of CONFIG Pin */
-int fpga_config_fn(int assert_config, int flush, int cookie)
+static int fpga_config_fn(int assert_config, int flush, int cookie)
 {
+
 	if (assert_config) {
 		gpio_set_value(GPIO_FPGA_CONFIGn, 0);
 	} else {
 		gpio_set_value(GPIO_FPGA_CONFIGn, 1);
 	}
+
 	return FPGA_SUCCESS;
 }
 
 /* Returns the state of STATUS Pin */
-int fpga_status_fn(int cookie)
+static int fpga_status_fn(int cookie)
 {
 	if (!gpio_get_value(GPIO_FPGA_STATUSn)) {
 		printf("STATUS = LOW\n");
-		return FPGA_FAIL;
+		return 0;
 	}
 	printf("STATUS = HIGH\n");
-	return FPGA_SUCCESS;
+	return 1;
 }
 
 /* Returns the state of CONF_DONE Pin */
-int fpga_done_fn(int cookie)
+static int fpga_done_fn(int cookie)
 {
-	if (!gpio_get_value(GPIO_FPGA_DONE)) {
-		printf("CONF_DON = LOW\n");
-		return FPGA_FAIL;
-	}
-	printf("CONF_DON = HIGH\n");
-	return FPGA_SUCCESS;
+	return gpio_get_value(GPIO_FPGA_DONE);
+
 }
 
+static int fpga_write_bitbang(const void *buf, size_t len, int flush, int cookie)
+{
+	int ret, n;
+	const uint8_t *ptr = buf;
+	uint8_t dat;
+	int clk = 0;
 
+	gpio_direction_output(IMX_GPIO_NR(5, 13), 0);
+	gpio_direction_output(IMX_GPIO_NR(5, 10), 0);
+
+	for (ret = 0; ret < len; ret++) {
+		dat = ptr[ret];
+		for (n=0; n < 8; n++) {
+			gpio_set_value(IMX_GPIO_NR(5, 10), (dat & (1 << n)) ? 1 : 0);
+			clk = clk ? 0 : 1;
+			gpio_set_value(IMX_GPIO_NR(5, 13), clk);
+		}
+	}
+	gpio_set_value(IMX_GPIO_NR(5, 13), 1);
+	gpio_set_value(IMX_GPIO_NR(5, 10), 1);
+	return 0;
+
+}
 /* writes the complete buffer to the FPGA
    writing the complete buffer in one function is much faster,
    then calling it for every bit */
-int fpga_write_fn(const void *buf, size_t len, int flush, int cookie)
+static int fpga_write_fn(const void *buf, size_t len, int flush, int cookie)
 {
 #define SPI2_BUS 1
 #define SPI2_CS0_FPGA_CONFIG 0
 #define SPI2_BUS_FREQ	20000000
 #define BIT_LENGTH	8
-	// TODO: Verify that u-bout counts from 0 (SPI2 = bus 1)
-	// TODO: Verify that FPGA support SPI_MODE_0
+	const uint8_t *ptr = buf;
 	struct spi_slave * spi_slave;
 	size_t bytecount = 0;
-	uint8_t *data = (uint8_t *) buf;
-	uint8_t val = 0;
-	uint8_t ret = 0;
+	int ret = 0;
+	unsigned long flags = 0;
 
 	/* Slave setup */
-	spi_slave = spi_setup_slave(SPI2_BUS, SPI2_CS0_FPGA_CONFIG,
-				SPI2_BUS_FREQ, SPI_MODE_0);
+	spi_slave = spi_setup_slave(SPI2_BUS, SPI2_CS0_FPGA_CONFIG | (IMX_GPIO_NR(5,12) << 8),
+				SPI2_BUS_FREQ, SPI_MODE_0 | SPI_LSB_FIRST);
 
+	if (spi_slave == NULL) {
+		printf("%s: spi_setup_slave returns NULL\n", __func__);
+		return -EINVAL;
+	}
+	printf("OCAS FPGA write ... buffer at 0x%08x, size = %d\n", (u32)ptr, len);
 	spi_claim_bus(spi_slave);
+
+	udelay(100);
 
 	/* Transfer data */
 	while (bytecount < len) {
-			val = data[bytecount];
-			ret = spi_xfer(spi_slave, BIT_LENGTH, &val, NULL, SPI_XFER_BEGIN | SPI_XFER_END);
-			if (ret != 0) {
-				printf("ERROR: Spi xfer failed, at byte cnt: %d!\n", bytecount);
-				break;
-			}
-			bytecount++;
+		ret = spi_xfer(spi_slave, BIT_LENGTH*4, (ptr+bytecount), NULL, flags);
+		if (ret != 0) {
+			printf("ERROR: Spi xfer failed, at byte cnt: %d!, err = %d\n", bytecount, ret);
+			break;
+		}
+		bytecount += 4;
+		if (bytecount % 0x10000 == 0)
+			printf(".");
 	}
 
 	spi_release_bus(spi_slave);
@@ -147,13 +163,13 @@ int fpga_write_fn(const void *buf, size_t len, int flush, int cookie)
 }
 
 /* called, when programming is aborted */
-int fpga_abort_fn(int cookie)
+static int fpga_abort_fn(int cookie)
 {
 	return FPGA_SUCCESS;
 }
 
 /* called, when programming was succesful */
-int fpga_post_fn(int cookie)
+static int fpga_post_fn(int cookie)
 {
 	return fpga_abort_fn(cookie);
 }
@@ -166,7 +182,7 @@ Altera_CYC2_Passive_Serial_fns fpga_fns = {
 	fpga_config_fn,
 	fpga_status_fn,
 	fpga_done_fn,
-	fpga_write_fn,
+	fpga_write_bitbang,
 	fpga_abort_fn,
 	fpga_post_fn
 };
@@ -174,7 +190,7 @@ Altera_CYC2_Passive_Serial_fns fpga_fns = {
 Altera_desc fpga[CONFIG_FPGA_COUNT] = {
 	{Altera_CYC2,
 	 passive_serial,
-	 Altera_EP2C35_SIZE,
+	 0x6b217c,
 	 (void *) &fpga_fns,
 	 NULL,
 	 0}
