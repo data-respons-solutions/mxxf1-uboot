8776124f6b41a06d5ef5e6456dfaa550e5a570b9
Hans Christian Lonstad <hcl@datarespons.no>
2015-09-22 12:26:55 +0200

    LVSBOOT-8: Prepare code for frame buffer support

--- board/datarespons/simpad2/simpad2.c
+++ board/datarespons/simpad2/simpad2.c
@@ -28,7 +28,7 @@
 #include <i2c.h>
 #include <power/pmic.h>
 #include <power/pfuze100_pmic.h>
-//#include "../common/pfuze.h"
+
 
 #include <usb.h>
 #include <pwm.h>
@@ -37,6 +37,8 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
+#define FB_SYNC_DATA_INVERT	0x20000000
+#define FB_SYNC_CLK_LAT_FALL	0x40000000
 #define USE_PWM_FOR_BL
 #define BL_PWM 0
 #define VIBRA_PWM 1
@@ -55,7 +57,6 @@ struct fsl_esdhc_cfg usdhc_cfg[2] = {
 	{USDHC3_BASE_ADDR},
 	{USDHC4_BASE_ADDR},
 };
-static int enable_display=0;
 
 #ifndef CONFIG_SPL_BUILD
 static const char* hw_string[8] = {
@@ -198,146 +199,20 @@ int board_phy_config(struct phy_device *phydev)
 	return 0;
 }
 
-
-#if !defined(CONFIG_SPL_BUILD) && defined(CONFIG_VIDEO_IPUV3) && defined(CONFIG_EMU_SABRESD)
-static void disable_lvds(struct display_info_t const *dev)
-{
-	struct iomuxc *iomux = (struct iomuxc *)IOMUXC_BASE_ADDR;
-
-	int reg = readl(&iomux->gpr[2]);
-
-	reg &= ~(IOMUXC_GPR2_LVDS_CH0_MODE_MASK |
-		 IOMUXC_GPR2_LVDS_CH1_MODE_MASK);
-
-	writel(reg, &iomux->gpr[2]);
-}
-
-static void do_enable_hdmi(struct display_info_t const *dev)
-{
-	disable_lvds(dev);
-	imx_enable_hdmi_phy();
-}
-
-static void enable_lvds(struct display_info_t const *dev)
-{
-	struct iomuxc *iomux = (struct iomuxc *)
-				IOMUXC_BASE_ADDR;
-	u32 reg = readl(&iomux->gpr[2]);
-	reg |= IOMUXC_GPR2_DATA_WIDTH_CH0_24BIT |
-	       IOMUXC_GPR2_DATA_WIDTH_CH1_24BIT;
-	writel(reg, &iomux->gpr[2]);
-}
-
-
-struct display_info_t const displays[] = {{
-	.bus	= -1,
-	.addr	= 0,
-	.pixfmt	= IPU_PIX_FMT_RGB666,
-	.detect	= NULL,
-	.enable	= enable_lvds,
-	.mode	= {
-			.name           = "simpad2-XGA",
-			.refresh        = 60,
-			.xres           = 1024,
-			.yres           = 768,
-			.pixclock       = 15385,
-			.left_margin    = 220,
-			.right_margin   = 40,
-			.upper_margin   = 21,
-			.lower_margin   = 7,
-			.hsync_len      = 60,
-			.vsync_len      = 10,
-			.sync           = FB_SYNC_EXT,
-			.vmode          = FB_VMODE_NONINTERLACED
-} }, {
-	.bus	= -1,
-	.addr	= 0,
-	.pixfmt	= IPU_PIX_FMT_RGB24,
-	.detect	= detect_hdmi,
-	.enable	= do_enable_hdmi,
-	.mode	= {
-			.name           = "HDMI",
-			.refresh        = 60,
-			.xres           = 1280,
-			.yres           = 720,
-			.pixclock       = 15686,
-			.left_margin    = 48,
-			.right_margin   = 32,
-			.upper_margin   = 3,
-			.lower_margin   = 5,
-			.hsync_len      = 80,
-			.vsync_len      = 13,
-			.sync           = FB_SYNC_EXT,
-			.vmode          = FB_VMODE_NONINTERLACED
-} },  };
-size_t display_count = ARRAY_SIZE(displays);
-
-static void setup_display(void)
-{
-	struct mxc_ccm_reg *mxc_ccm = (struct mxc_ccm_reg *)CCM_BASE_ADDR;
-	struct iomuxc *iomux = (struct iomuxc *)IOMUXC_BASE_ADDR;
-	int reg;
-
-	/* Setup HSYNC, VSYNC, DISP_CLK for debugging purposes */
-	imx_iomux_v3_setup_multiple_pads(di0_pads, ARRAY_SIZE(di0_pads));
-
-	enable_ipu_clock();
-	imx_setup_hdmi();
-
-	/* Turn on LDB0, LDB1, IPU,IPU DI0 clocks */
-	reg = readl(&mxc_ccm->CCGR3);
-	reg |=  MXC_CCM_CCGR3_LDB_DI0_MASK | MXC_CCM_CCGR3_LDB_DI1_MASK;
-	writel(reg, &mxc_ccm->CCGR3);
-
-	/* set LDB0, LDB1 clk select to 011/011 */
-	reg = readl(&mxc_ccm->cs2cdr);
-	reg &= ~(MXC_CCM_CS2CDR_LDB_DI0_CLK_SEL_MASK
-		 | MXC_CCM_CS2CDR_LDB_DI1_CLK_SEL_MASK);
-	reg |= (3 << MXC_CCM_CS2CDR_LDB_DI0_CLK_SEL_OFFSET)
-	      | (3 << MXC_CCM_CS2CDR_LDB_DI1_CLK_SEL_OFFSET);
-	writel(reg, &mxc_ccm->cs2cdr);
-
-	reg = readl(&mxc_ccm->cscmr2);
-	reg |= MXC_CCM_CSCMR2_LDB_DI0_IPU_DIV | MXC_CCM_CSCMR2_LDB_DI1_IPU_DIV;
-	writel(reg, &mxc_ccm->cscmr2);
-
-	reg = readl(&mxc_ccm->chsccdr);
-	reg |= (CHSCCDR_CLK_SEL_LDB_DI0
-		<< MXC_CCM_CHSCCDR_IPU1_DI0_CLK_SEL_OFFSET);
-	reg |= (CHSCCDR_CLK_SEL_LDB_DI0
-		<< MXC_CCM_CHSCCDR_IPU1_DI1_CLK_SEL_OFFSET);
-	writel(reg, &mxc_ccm->chsccdr);
-
-	reg = IOMUXC_GPR2_BGREF_RRMODE_EXTERNAL_RES
-	     | IOMUXC_GPR2_DI1_VS_POLARITY_ACTIVE_LOW
-	     | IOMUXC_GPR2_DI0_VS_POLARITY_ACTIVE_LOW
-	     | IOMUXC_GPR2_BIT_MAPPING_CH1_SPWG
-	     | IOMUXC_GPR2_DATA_WIDTH_CH1_18BIT
-	     | IOMUXC_GPR2_BIT_MAPPING_CH0_SPWG
-	     | IOMUXC_GPR2_DATA_WIDTH_CH0_18BIT
-	     | IOMUXC_GPR2_LVDS_CH0_MODE_DISABLED
-	     | IOMUXC_GPR2_LVDS_CH1_MODE_ENABLED_DI0;
-	writel(reg, &iomux->gpr[2]);
-
-	reg = readl(&iomux->gpr[3]);
-	reg = (reg & ~(IOMUXC_GPR3_LVDS1_MUX_CTL_MASK
-			| IOMUXC_GPR3_HDMI_MUX_CTL_MASK))
-	    | (IOMUXC_GPR3_MUX_SRC_IPU1_DI0
-	       << IOMUXC_GPR3_LVDS1_MUX_CTL_OFFSET);
-	writel(reg, &iomux->gpr[3]);
-}
-#endif
-
-#if !defined(CONFIG_SPL_BUILD) && defined(CONFIG_VIDEO_IPUV3) && !defined(CONFIG_EMU_SABRESD)
+#if !defined(CONFIG_SPL_BUILD) && defined(CONFIG_VIDEO_IPUV3)
 
 static void enable_rgb(struct display_info_t const *dev)
 {
+	imx_iomux_v3_setup_multiple_pads(rgb_pads, ARRAY_SIZE(rgb_pads));
+	pwm_enable(BL_PWM);
+	gpio_set_value(GPIO_LCD_EN, 1);
+	gpio_set_value(GPIO_BL_EN, 1);
 }
 
 struct display_info_t const displays[] = {{
 	.bus	= -1,
 	.addr	= 0,
-	.pixfmt	= IPU_PIX_FMT_RGB666,
+	.pixfmt	= IPU_PIX_FMT_RGB565,
 	.detect	= NULL,
 	.enable	= enable_rgb,
 	.mode	= {
@@ -345,93 +220,90 @@ struct display_info_t const displays[] = {{
 			.refresh        = 60,
 			.xres           = 640,
 			.yres           = 480,
-			.pixclock       = 41500,
-			.left_margin    = 220,
-			.right_margin   = 40,
-			.upper_margin   = 21,
-			.lower_margin   = 7,
-			.hsync_len      = 60,
-			.vsync_len      = 10,
-			.sync           = FB_SYNC_EXT,
+			.pixclock       = 40000,
+			.left_margin    = 16,
+			.right_margin   = 114,
+			.upper_margin   = 10,
+			.lower_margin   = 32,
+			.hsync_len      = 30,
+			.vsync_len      = 3,
+			.sync           = FB_SYNC_CLK_LAT_FALL,
 			.vmode          = FB_VMODE_NONINTERLACED
 } },  };
+
 size_t display_count = ARRAY_SIZE(displays);
-static void setup_display(void)
+
+static void enable_vpll(void)
 {
+	struct mxc_ccm_reg *ccm = (struct mxc_ccm_reg *)CCM_BASE_ADDR;
+	int timeout = 100000;
+
+	setbits_le32(&ccm->analog_pll_video, BM_ANADIG_PLL_VIDEO_POWERDOWN);
+
+	clrsetbits_le32(&ccm->analog_pll_video,
+			BM_ANADIG_PLL_VIDEO_DIV_SELECT |
+			BM_ANADIG_PLL_VIDEO_POST_DIV_SELECT,
+			BF_ANADIG_PLL_VIDEO_DIV_SELECT(37) |
+			BF_ANADIG_PLL_VIDEO_POST_DIV_SELECT(1));
+
+	writel(BF_ANADIG_PLL_VIDEO_NUM_A(11), &ccm->analog_pll_video_num);
+	writel(BF_ANADIG_PLL_VIDEO_DENOM_B(12), &ccm->analog_pll_video_denom);
+
+	clrbits_le32(&ccm->analog_pll_video, BM_ANADIG_PLL_VIDEO_POWERDOWN);
+
+	while (timeout--)
+		if (readl(&ccm->analog_pll_video) & BM_ANADIG_PLL_VIDEO_LOCK)
+			break;
+	if (timeout < 0)
+		printf("Warning: video pll lock timeout!\n");
+
+	clrsetbits_le32(&ccm->analog_pll_video,
+			BM_ANADIG_PLL_VIDEO_BYPASS,
+			BM_ANADIG_PLL_VIDEO_ENABLE);
+}
+
+static void setup_display_clock(void)
+{
+	struct mxc_ccm_reg *mxc_ccm = (struct mxc_ccm_reg *)CCM_BASE_ADDR;
+	struct iomuxc *iomux = (struct iomuxc *)IOMUXC_BASE_ADDR;
+
+	enable_ipu_clock();
+	enable_vpll();
+	imx_setup_hdmi();
+
+	/* Turn on IPU LDB DI0 clocks */
+	setbits_le32(&mxc_ccm->CCGR3, MXC_CCM_CCGR3_IPU1_IPU_DI0_MASK);
+
+	/* DI0 clock derived from ldb_di0_clk */
+	clrsetbits_le32(&mxc_ccm->chsccdr,
+			MXC_CCM_CHSCCDR_IPU1_DI0_CLK_SEL_MASK,
+			(1 << MXC_CCM_CHSCCDR_IPU1_DI0_CLK_SEL_OFFSET)
+			);
+
+	/* Enable both LVDS channels, both connected to DI0. */
+	writel(IOMUXC_GPR2_DI0_VS_POLARITY_ACTIVE_LOW,
+	       &iomux->gpr[2]);
 
 }
-#endif
 
-#ifdef CONFIG_VIDEO
+static void setup_display(void)
+{
+	struct mxc_ccm_reg *mxc_ccm = (struct mxc_ccm_reg *)CCM_BASE_ADDR;
+	struct iomuxc *iomux = (struct iomuxc *)IOMUXC_BASE_ADDR;
+	int reg;
+
+	setup_display_clock();
+
+}
+
 int overwrite_console(void)
 {
-	return 0;
+	return 1;
 }
 #endif
 
 
 #ifndef CONFIG_SPL_BUILD
-#define MAX_I2C_DATA_LEN 10
-static char egalax_fw[6];
-static int egalax_firmware_version(void)
-{
-	int to=100000;
-	static const uint8_t cmd[MAX_I2C_DATA_LEN] = { 0x03, 0x03, 0xa, 0x01, 'D', 0, 0, 0, 0, 0 };
-	uint8_t rcv_buf[MAX_I2C_DATA_LEN+1];
-
-	int ret;
-	i2c_set_bus_num(2);
-	i2c_set_bus_speed(CONFIG_SYS_I2C_SPEED);
-
-	ret = i2c_probe(0x04);
-	if (ret)
-	{
-		printf("%s: NO touch controller\n", __func__);
-		return -ENODEV;
-	}
-	if (i2c_write(0x04, 0, 0, (uint8_t*)cmd, MAX_I2C_DATA_LEN))
-	{
-		printf("%s: Error requesting FW version\n", __func__);
-		return -EIO;
-	}
-	else
-	{
-		while (gpio_get_value(GPIO_TOUCH_IRQ) && to > 0)
-		{
-			udelay(1000);
-			to--;
-		}
-		if (to <= 0)
-		{
-			printf("%s: timeout waiting for touch irq\n", __func__);
-			return -EIO;
-		}
-		ret = i2c_read(0x04, 0, 0, rcv_buf, MAX_I2C_DATA_LEN);
-		if (ret)
-		{
-			printf("%s: Error receiving FW version\n", __func__);
-			return -EIO;
-		}
-		if (rcv_buf[0] == 0x03 && rcv_buf[4] == 'D')
-		{
-			rcv_buf[MAX_I2C_DATA_LEN] = '\0';
-			strcpy(egalax_fw, (char *)&rcv_buf[5]);
-			printf("Touch FW version [%s]\n", egalax_fw);
-			i2c_read(0x04, 0, 0, rcv_buf, MAX_I2C_DATA_LEN);	/* Drain the rest */
-		}
-		else
-		{
-			printf("%s: Got touch message %x.%x.%x\n", __func__, rcv_buf[0], rcv_buf[1], rcv_buf[2]);
-		}
-	}
-
-#ifndef CONFIG_EMU_SABRESD
-	gpio_set_value(GPIO_CAP_TOUCH_RST, 0);
-	udelay(500);
-	gpio_set_value(GPIO_CAP_TOUCH_RST, 1);
-#endif
-	return 0;
-}
 
 static void setup_iomux_enet(void)
 {
@@ -443,10 +315,6 @@ static void setup_iomux_enet(void)
 	gpio_set_value(IMX_GPIO_NR(1, 25), 1);
 }
 
-
-int simpad2_eeprom_init (unsigned dev_addr);
-int eeprom_get_mac_addr(void);
-int eeprom_addr;
 
 int board_eth_init(bd_t *bis)
 {
@@ -554,7 +422,7 @@ int board_early_init_f(void)
 #endif
 
 #if !defined(CONFIG_SPL_BUILD) && defined(CONFIG_VIDEO)
-	setup_display();
+	imx_iomux_v3_setup_multiple_pads(rgb_pads, ARRAY_SIZE(rgb_pads));
 #endif
 	return 0;
 }
@@ -585,19 +453,11 @@ int board_init(void)
 	gpio_direction_input(GPIO_TOUCH_IRQ);
 
 #endif
-	/* Check if the display should present u-boot info */
-	enable_display = gpio_get_value(KEY_FUNCTION);	/* TODO: invert */
-
-	if (enable_display)
-	{
-		if (pwm_config(BL_PWM, 4000, 5000))
-			printf("%s: pwm_config for backlight ERROR\n", __func__);
-
-		pwm_enable(BL_PWM);
-		gpio_set_value(GPIO_LCD_EN, 1);
-		gpio_set_value(GPIO_BL_EN, 1);
-	}
-
+#ifdef CONFIG_VIDEO_IPUV3
+	setup_display();
+	if (pwm_config(BL_PWM, 2000, 5000))
+		printf("%s: pwm_config for backlight ERROR\n", __func__);
+#endif
 	setup_usb();
 	return 0;
 }
@@ -659,16 +519,7 @@ int board_late_init(void)
 	printf("SIMPAD2 HW version: %s\n", hw_string[version]);
 
 #endif
-	//egalax_firmware_version();
-#if 0
-	printf("Simpad2 U-BOOT version [%s]\n", U_BOOT_VERSION);
-	if (egalax_firmware_version() == 0)
-		vpd_update(egalax_fw, GPIO_SPI_NOR_WP);
-	else
-		vpd_update(0, GPIO_SPI_NOR_WP);
-#endif
 	cmd_process(0, 2, usbcmd, &rep, &ticks);
-	//eeprom_get_mac_addr();
 
 #ifdef CONFIG_CMD_BMODE
 	add_board_boot_modes(board_boot_modes);
